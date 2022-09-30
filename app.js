90cc5a4ee07f3a25c2a2c52e8a7d61c89071ebd2
vblock818 <65957364+vblock818@users.noreply.github.com>
2022-09-30 16:25:09 +0200

    routing

---
+++ app.js
@@ -0,0 +1,25 @@
+// const http = require("http");
+
+// const server = http.createServer((request,response) => {
+//     response.statusCode = 200;
+//     response.end('<h1>Hallo World</h1>');
+// });
+
+// server.listen(3000);
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('<h1>Hello World</h1><p>lorem</p>')
+})
+
+app.get('/about', function (req, res) {
+    res.send('<h1>Hello About</h1><p>lorem ipsum</p>')
+  })
+
+  app.get('/impresum', function (req, res) {
+    res.send('<h1>Hello impresum</h1><p>lorem ipsum dolore</p>')
+  })
+
+app.listen(3000)
